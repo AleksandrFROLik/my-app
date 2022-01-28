fb19c3e514b5bd516cc8d8c143200da0677e7990
AleksandrFROLik <nextfrolik@gmail.com>
2022-01-28 22:13:16 +0300

    portfolio path 2 start path 2

--- src/components/works/Works.tsx
+++ src/components/works/Works.tsx
@@ -1,20 +1,29 @@
 import React from "react";
-import styles from './Works.module.css'
+import styles from './Works.module.scss'
 import stylesContainer from '../stylesComponent/styles/stylesContainer.module.css'
 import {Work} from "./work/Work";
 import {Title} from "../common/title/Title";
+import todolist from '../../assets/image/todolist.jpg'
+import social from '../../assets/image/social.jpg'
+
 
 
 export const Works = () => {
+    const socialImage = {
+        backgroundImage: `url(${social})`
+    }
+    const todolistImage = {
+        backgroundImage: `url(${todolist})`
+    }
     return (
         <div className={styles.worksBlock}>
             <div className={`${stylesContainer.container} ${styles.worksContainer}`}>
                 <Title title='works'/>
                 <div className={styles.works}>
-                    <Work/>
-                    <Work/>
-                    <Work/>
-                    <Work/>
+                    <Work style={socialImage} description="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."/>
+                    <Work style={todolistImage} description="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."/>
+                    <Work style={socialImage} description="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."/>
+                    <Work style={todolistImage} description="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."/>
                 </div>
             </div>
 
--- src/components/works/work/Work.tsx
+++ src/components/works/work/Work.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import styles from './Work.module.css'
 
+type WorkType = {
+    description: string
+    style:{backgroundImage:string}
+}
 
-export const Work = () => {
+export const Work = ({description, style}:WorkType) => {
     return (
 
         <div className={styles.workBlock}>
-            <div className={styles.imageBlock}>
+            <div className={styles.imageBlock} style={style}>
                 <div className={styles.button}>
                     <a href="/">Click</a>
                 </div>
             </div>
-            <h3 className={styles.workTitle}>Name project</h3>
-            <span className={styles.description}>Short description</span>
+            <div className={styles.workInfo}>
+                <h3 className={styles.workTitle}>Name project</h3>
+                <span className={styles.description}>{description}</span>
+            </div>
+
         </div>
 
 
